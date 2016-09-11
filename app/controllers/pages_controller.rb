8a40372eaa7277df49a5e0aa5fd6eb72188a319b
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2016-09-11 19:34:14 +0200

    Sign in with authorization and session helpers

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  before_action :authorize
+
+  def welcome
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get '/sign_up', to: 'users#new', as: 'sign_up'
-  post '/sign_up', to: 'users#create'
+  root 'pages#welcome'
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+
+  get '/signup', to: 'users#new', as: 'signup'
+  post '/signup', to: 'users#create'
 end
