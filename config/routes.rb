53ce56c71770568e670ee1c85b2a2002d2752fdd
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2016-09-11 15:35:07 +0200

    Finished user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/sign_up', to: 'users#new', as: 'sign_up'
+  post '/sign_up', to: 'users#create'
 end
